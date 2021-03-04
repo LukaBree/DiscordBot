91ee6b043aad1cf87cb58c6f482196f7f52db756
LukaBree <filipovic0204@gmail.com>
2021-03-04 12:04:38 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,15 +2,18 @@ module.exports = {
     name: 'info',
     description: "Govori Info",
     execute(message, args, Discord){
-        const newEmbed = new Discord.MessageEmbed()
+        const info = new Discord.MessageEmbed()
         .setColor('0x950765')
         .setTitle('INFO')
-        .setDescription('resi <@394169972806647812>')
+        .setDescription('Staff Team na ovom discordu')
         .addFields(
-            {name: 'Info', value: 'test'},
-
+            {name: 'urke98#6886', value: 'Vlasnik'},
+            {name: 'Lukaa#1317', value: 'Glavni Admin'},
+            {name: 'vuk77765#1694', value: 'Moderator'},
         )
+        .setTimestamp()
+        .setFooter('Bota napravio Lukaa#1317');
 
-        message.channel.send(newEmbed)
+        message.channel.send(info)
     }
 }
