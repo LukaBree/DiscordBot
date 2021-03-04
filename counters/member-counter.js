d634f6e1e7998d64e10ec9677fdef5f30ee22f8f
LukaBree <filipovic0204@gmail.com>
2021-03-04 11:07:22 +0100

    Update member-counter.js

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,5 +1,5 @@
 module.export = async (bot) =>{
-    const guild = client.guilds.cache.get('816420776437088296');
+    const guild = bot.guilds.cache.get('816420776437088296');
     setInterval(() =>{
         const memberCount = guild.memberCount;
         const channel = guild.channels.cache.get('816972303359016990');
